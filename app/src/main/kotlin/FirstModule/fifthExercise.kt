61f4c185b1326f7df073e0918ef830997f0457c4
Uliana Golper <uliagolper@gmail.com>
2023-10-22 16:13:25 +0300

    два файла сразу не загрузились

--- app/src/main/kotlin/FirstModule/fifthExercise.kt
+++ app/src/main/kotlin/FirstModule/fifthExercise.kt
@@ -1,2 +1,11 @@
-package FirstModule
+package FifthModule
 
+fun main() {
+    /**  тут могла бы быть ваша документация
+     * и тут
+     */
+
+    println("Введите любимое имя котика:")
+    val input = readLine() // Считывает строку из стандартного ввода
+    println("Ваше любимое имя: $input")
+}
--- app/src/main/kotlin/FirstModule/forthExercise.kt
+++ app/src/main/kotlin/FirstModule/forthExercise.kt
@@ -1,2 +1,19 @@
 package FirstModule
 
+var a = 10
+val b = 20
+var c = 30
+
+val diff = a - b
+val isNotEqual = c!= b
+val condition= (a > b) || (a > c)
+
+fun main() {
+    println("сумма а+b = ${a+b}")
+    println("разница a-b = $diff")
+    println("c не равно b: $isNotEqual")
+    println("одно из условий верно: $condition")
+    c += 5
+    println("c= $c")
+
+}
