c43f05853e65e7a3d54cb7d8e05fbfdfe55abf73
Uliana Golper <uliagolper@gmail.com>
2023-10-29 12:40:31 +0300

    добавлены условия задач

--- app/src/main/kotlin/SecondModule/M2EX1.kt
+++ app/src/main/kotlin/SecondModule/M2EX1.kt
@@ -2,45 +2,47 @@ package SecondModule
 
 //Управляющие конструкции. Условные операторы:
 //Напишите программу, которая принимает целое число от пользователя и проверяет, является ли оно положительным, отрицательным или нулем. Используйте различные варианты условных операторов (if, if-else, when).
-var catsCount: Int = 5
+
 
 fun main() {
-    if (catsCount == 5) {
+    var catsCountSM: Int = 5
+    if (catsCountSM == 5) {
         println("У Ули 5 котиков")
     }
-    checkCatsCount()
-    checkCatsCount2()
-    checkCatsCount3()
-}
 
-fun checkCatsCount() {
-    catsCount = 2
-    if (catsCount <= 0) {
-        println("У Ули нет котов")
-    } else println("Новое количество котов: $catsCount")
+    fun checkCatsCount() {
+        catsCountSM = 2
+        if (catsCountSM <= 0) {
+            println("У Ули нет котов")
+        } else println("Новое количество котов: $catsCountSM")
 
-}
-
-fun checkCatsCount2() {
-    catsCount = 10
-    if (catsCount <= 0) {
-        println("У Ули нет котов")
-    } else if (catsCount <= 5) {
-        println("Оптимальное количество котов")
-    } else if (catsCount > 5)
-        println("Котов больше пяти")
-}
-
-fun checkCatsCount3() {
-    catsCount = 2
-    when (catsCount) {
-
-        0 -> println("У Ули нет котов")
-        1 -> println("Нормальное количество котов")
-        2 -> println("Приемлемое количество котов")
-        3 -> println("Оптимальное количество котов")
-        5 -> println("Котов многовато")
-        else -> println("Котов слишком много")
     }
+
+    fun checkCatsCount2() {
+        catsCountSM = 10
+        if (catsCountSM <= 0) {
+            println("У Ули нет котов")
+        } else if (catsCountSM <= 5) {
+            println("Оптимальное количество котов")
+        } else if (catsCountSM > 5)
+            println("Котов больше пяти")
+    }
+
+    fun checkCatsCount3() {
+        catsCountSM = 2
+        when (catsCountSM) {
+
+            0 -> println("У Ули нет котов")
+            1 -> println("Нормальное количество котов")
+            2 -> println("Приемлемое количество котов")
+            3 -> println("Оптимальное количество котов")
+            5 -> println("Котов многовато")
+            else -> println("Котов слишком много")
+        }
+    }
+
 }
+
+
+
 
--- app/src/main/kotlin/SecondModule/M2EX2.kt
+++ app/src/main/kotlin/SecondModule/M2EX2.kt
@@ -1,27 +1,30 @@
+//Циклы:
+//
+//Создайте массив из 10 чисел. Напишите цикл for, который будет выводить каждое число и его квадрат.
+//После этого напишите цикл while, который будет выводить каждое число и его куб.
+//Используйте функцию repeat для вывода строки “Привет, мир!” 10 раз.
 package SecondModule
 
-var catsNumber: Array<Int> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
 fun main() {
+    var catsNumberSM: Array<Int> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     println("Cначала выводятся квадраты чисел")
-    for (i in catsNumber) {
+    for (i in catsNumberSM) {
         println("квадрат $i = ${i * i}")
     }
-    cubes()
-    repeatGreetings()
-}
+    fun cubes() {
+        println("Тепеpь выводятся кубы чисел")
+        var i = 1
+        do {
+            println("Куб числа $i = ${i * i * i}")
+        } while (i++ < catsNumberSM.size)
+    }
 
-fun cubes() {
-    println("Тепеpь выводятся кубы чисел")
-    var i = 1
-    do {
-        println("Куб числа $i = ${i * i * i}")
-    } while (i++ < catsNumber.size)
-}
-
-fun repeatGreetings() {
-    println("И сейчас \"Привет, мир!\" выведется 10 раз")
-    repeat(10) {
-        println("Привет, мир!")
+    fun repeatGreetings() {
+        println("И сейчас \"Привет, мир!\" выведется 10 раз")
+        repeat(10) {
+            println("Привет, мир!")
+        }
     }
 }
 
